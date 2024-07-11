2e4912f0716d8d66b3888a18f800c97bea4d8c2c
Oatstar <76726117+Oatstar@users.noreply.github.com>
2024-07-11 18:15:58 +0300

    up

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -7,11 +7,13 @@ public class MonsterController : MonoBehaviour
     [SerializeField] int health = 100;
     int maxHealth = 100;
     [SerializeField] float speed = 5f; // Adjust this value to control the speed of movement
+    [SerializeField] GameObject sliderCanvasPrefab;
     [SerializeField] Slider slider;
     private Rigidbody2D rb;
-
+    
     void Start()
     {
+        Instantiate(sliderCanvasPrefab, transform.position, Quaternion.identity, this.transform);
         slider = GetComponentInChildren<Slider>();
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(speed, rb.velocity.y); // Initial velocity towards right
